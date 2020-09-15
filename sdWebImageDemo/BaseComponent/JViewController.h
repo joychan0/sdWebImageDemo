6dd62d8bb439cefcd3c3546de860b6f0e1b0a68d
江一川 <joychan0@163.com>
2020-09-15 17:39:15 +0800

    pod

---
+++ sdWebImageDemo/BaseComponent/JViewController.h
@@ -0,0 +1,16 @@
+//// JViewController.h
+// sdWebImageDemo
+//
+// Copyright © 2020 江一川. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sdWebImageDemo/BaseComponent/JViewController.m
@@ -0,0 +1,23 @@
+//// JViewController.m
+// sdWebImageDemo
+//
+// Copyright © 2020 江一川. All rights reserved.
+//
+
+
+#import "JViewController.h"
+
+@interface JViewController ()
+
+@end
+
+@implementation JViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
